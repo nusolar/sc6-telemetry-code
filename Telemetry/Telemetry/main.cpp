8f6ade41236bb2344b96ded8b46593e718bd7eae
Alex Chandel <alexchandel@gmail.com>
2012-11-04 12:52:45 -0600

    setup telemetry project
    it's in xcode

---
+++ Telemetry/Telemetry/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Telepo
+//
+//  Created by Alex Chandel on 10/28/12.
+//  Copyright (c) 2012 Alex Chandel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
---
+++ Telepo/Telepo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Telepo
+//
+//  Created by Alex Chandel on 10/28/12.
+//  Copyright (c) 2012 Alex Chandel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
