7b6de9ffef095681268ca7b804df5f014b2767be
Alex Chandel <alex@wireless-165-124-116-122.nuwlan.northwestern.edu>
2013-01-22 14:49:51 -0600

    Make car compile
    This works, but fails (and kernel panics!) if anything goes wrong. Will
    implement try/catch flow later (the plan is outlined in an inline
    comment)

--- car.py
+++ car.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import serial, io, time, pika, os, sys
-
-def listen:
+# flow: loop( not_serial.serialutil.SerialException(loop( not_pika.exceptions.AMQPConnectionError(send) wait, reconnect)) wait, recheck)
+def listen():
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='mbr.chandel.net'))
 	channel = connection.channel()
 	channel.queue_declare(queue='telemetry')
 	
 	ser = serial.Serial(device())
-	if !ser.isOpen():
+	if not ser.isOpen():
 		print("Error: couldn't open serial connection!")
 	ser.write('S8\rO\r')
 	
@@ -23,8 +23,8 @@ def listen:
 	ser.close()
 	connection.close()
 
-def device:
+def device():
 	files = {'darwin':'/dev/tty.usbserial-LWR8N2L2', 'linux2':'/dev/ttyUSB1'}
-	if !os.path.exists(file[sys.platform]):
+	if not os.path.exists(files[sys.platform]):
 		print("Error: device file doesn't exist!")
 	return files[sys.platform]
