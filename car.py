589af22bf0901e213b67de1a03e06be6e2a221f5
Alex Chandel <alex@alex-mbr.northwestern.edu>
2013-01-22 22:25:02 -0600

    error checking for car telemetry
    Infinite loops that ignore Network and Serial exceptions, with 5 second
    gap between retries.

--- car.py
+++ car.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 import serial, io, time, pika, os, sys
-# flow: loop( not_serial.serialutil.SerialException(loop( not_pika.exceptions.AMQPConnectionError(send) wait, reconnect)) wait, recheck)
-def listen():
+
+def loop(fun):
+	while True:
+		try: fun()
+		except: pass
+		time.sleep(5)
+
+def listen(ser):
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='mbr.chandel.net'))
 	channel = connection.channel()
 	channel.queue_declare(queue='telemetry')
 	
-	ser = serial.Serial(device())
-	if not ser.isOpen():
-		print("Error: couldn't open serial connection!")
+	if not ser.isOpen(): print("WTF: %s isn't open" % ser.port)
 	ser.write('S8\rO\r')
-	
 	sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 	while sio.readable():
 		line = sio.readline()
-		if line[0] != 't':
-			continue
+		if line[0] != 't': continue
 		line = str(time.time()) + line
-		channel.basic_publish(exchange='',routing_key='telemetry',body=packet)
+		channel.basic_publish(exchange='',routing_key='telemetry',body=line)
 	
 	sio.close()
 	ser.close()
@@ -25,6 +27,8 @@ def listen():
 
 def device():
 	files = {'darwin':'/dev/tty.usbserial-LWR8N2L2', 'linux2':'/dev/ttyUSB1'}
-	if not os.path.exists(files[sys.platform]):
-		print("Error: device file doesn't exist!")
-	return files[sys.platform]
+	ser = serial.Serial(files[sys.platform])
+	loop(lambda: listen(ser))
+
+if __name__ == '__main__':
+	loop(device)
