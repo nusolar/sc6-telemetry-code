40f7129b809973588cf973790b72bbeffbfe961b
Alex Chandel <alex@alex-mbr.northwestern.edu>
2013-02-10 10:22:26 -0600

    more data extraction, not finished
    a few circuit items could be given special module #'s and tabled with
    the batteries. SQL is currently called from analytics; we should bring
    it back to the callback

--- consumer.py
+++ consumer.py
@@ -14,32 +14,36 @@ tabulae = ((('trip',), 'trips'),
 		   (('horn','signals','cruise','lights'), 'carinfo'),
 		   (('',), 'other'))
 
-def desc(time, name, data): # [4 char, uint32]
+def descr(time, name, data): #[4 char, uint32]
 	row = (time, name, data[0:8].decode('hex'), int(data[8:],16))
 	con.execute("INSERT INTO descr VALUES (?,?,?,?)", row)
+def trips(time, name, data): #TODO
+	pass
 def modules(time, name, data): #assume [uint32, float], 16 CHAR TOTAL!!!
 	row = (time, name, int(data[0:8],16), float.fromhex(data[8:]))
 	con.execute("INSERT INTO modules VALUES (?,?,?,?)", row)
-def circuit_d(time, name, data): #[double count]
-	pass
-def can_bms_tx_current(time, name, data): #[float array, float batt]
-	pass
-def commands(time, name, data): #TODO
-	pass
+def circuit_d(time, cid, data): #[double count]
+	pass #could add to modules table
+def can_bms_tx_current(time, cid, data): #[float array, float batt]
+	pass #+-> modules table?
+def sw(time, cid, data): pass #COMPLICATED TODO
+def cmds(time, cid, data): #TODO [float, float]?
+	row = (time, name, float.fromhex(data[0:8]), float.fromhex(data[8:]))
+	con.execute("INSERT INTO cmds VALUES (?,?,?,?)", row) 
 def motor(time, name, data): #[float Re, float Im]
 	row = (time, name, float.fromhex(data[0:8]), float.fromhex(data[8:]))
 	con.execute("INSERT INTO motor VALUES (?,?,?,?)", row)
 def motor_swapped(time, name, data): #[float Im, float Re]
 	row = (time, name, float.fromhex(data[8:]), float.fromhex(data[0:8]))
 	con.execute("INSERT INTO motor VALUES (?,?,?,?)", row)
-def mppt():pass
-tables = ((('_heartbeat','_id'),desc),
+def mppt(): pass #COMPLICATED TODO
+tables = ((('_heartbeat','_id'),descr),
 		  (('bms_tx_voltage','bms_tx_owvoltage','bms_tx_temp'), modules),
 		  (('_uptime','_cc_array','_cc_batt','_wh_batt'), circuit_d),
 		  (('can_bms_tx_current',), can_bms_tx_current),
-		  (('_cmd',),commands),
-		  (('ws20_tx','e_current','tx_velocity'),motor),
-		  (('_phase','_vector','_backemf',),motor_swapped))
+		  (('_cmd', 'dc_rx_cruise_'),cmds),
+		  (('_phase','_vector','_backemf',),motor_swapped),
+		  (('ws20_tx_',),motor),)
 
 def receive():
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='mbr.chandel.net'))
