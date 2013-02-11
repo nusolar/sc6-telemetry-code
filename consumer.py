fd69ad69a9d84245d81f50cbae2f374e5de6fd09
Alex Chandel <alex@wireless-165-124-117-160.nuwlan.northwestern.edu>
2013-02-10 21:47:17 -0600

    name changes
    and others

--- consumer.py
+++ consumer.py
@@ -14,27 +14,27 @@ tabulae = ((('trip',), 'trips'),
 		   (('horn','signals','cruise','lights'), 'carinfo'),
 		   (('',), 'other'))
 
-def descr(time, name, data): #[4 char, uint32]
-	return ("descr", (time, name, data[0:8].decode('hex'), int(data[8:],16)))
-def trips(time, name, data): #TODO trip member orders
+def descr(time, addr, data): #[4 char, uint32]
+	return ("descr", (time, addr, data[0:8].decode('hex'), int(data[8:],16)))
+def trips(time, addr, data): #TODO trip member orders
 	pass
-def modules(time, name, data): #assume [uint32, float], 16 CHAR TOTAL!!!
-	return ("modules", time, name, int(data[0:8],16), float.fromhex(data[8:]))
-def circuit_d(time, cid, data): #[double count]
-	pass #could add to modules table
 def can_bms_tx_current(time, cid, data): #[float array, float batt]
 	pass #+-> modules table?
-def sw(time, cid, data):
+def modules(time, addr, data): #assume [uint32, float], 16 CHAR TOTAL!!!
+	return ("modules", (time, addr, int(data[0:8],16), float.fromhex(data[8:])))
+def circuit_d(time, addr, data): #[double count]
+	return ("modules", (time, addr, 0, float.fromhex(data)))
+def sw(time, addr, data):
 	pass #COMPLICATED TODO
-def cmds(time, cid, data): #[float, float] TODO
-	return ("cmds",  (time, name, float.fromhex(data[0:8]), float.fromhex(data[8:])))
-def motor(time, name, data): #[float Re, float Im]
-	return ("motor", (time, name, float.fromhex(data[0:8]), float.fromhex(data[8:])))
-def motor_swapped(time, name, data): #[float Im, float Re]
-	return ("motor", (time, name, float.fromhex(data[8:]), float.fromhex(data[0:8])))
-def mppt():
+def cmds(time, addr, data): #[float, float] TODO
+	return ("cmds",  (time, addr, float.fromhex(data[0:8]), float.fromhex(data[8:])))
+def motor(time, addr, data): #[float Re, float Im]
+	return ("motor", (time, addr, float.fromhex(data[0:8]), float.fromhex(data[8:])))
+def motor_swapped(time, addr, data): #[float Im, float Re]
+	return ("motor", (time, addr, float.fromhex(data[8:]), float.fromhex(data[0:8])))
+def mppt(time, addr, data):
 	pass #COMPLICATED TODO
-tables = ((('_heartbeat','_id'),descr),
+handlers = ((('_heartbeat','_id'),descr),
 		  (('bms_tx_voltage','bms_tx_owvoltage','bms_tx_temp'), modules),
 		  (('_uptime','_cc_array','_cc_batt','_wh_batt'), circuit_d),
 		  (('can_bms_tx_current',), can_bms_tx_current),
@@ -52,12 +52,12 @@ def receive():
 	def callback(ch, method, properties, pkt):
 		t = pkt.find('t')
 		v = [float(pkt[0:t]), int(pkt[t+1:t+4],16), int(pkt[t+4]), pkt[t+5:]]
-		for pair in tables:
+		for pair in handlers:
 			if any(x in db.name[v[1]] for x in pair[0]):
 				v.append(pair[1](v[0],v[1],v[3])) #(time, id, str)
-		v[4][1][1] = v[1]
-		con.execute("INSERT INTO %s VALUES (?,?,?,?)" % v[4][0], v[4][1])
-		con.commit()
+		if v[4][0] > '':
+			con.execute("INSERT INTO %s VALUES (?,?,?,?)" % v[4][0], v[4][1])
+			con.commit()
 		if halt: quit()
 	
 	channel.basic_consume(callback,queue='telemetry',no_ack=True)
