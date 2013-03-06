fe9d4be7b84ce9d779485aee1124723c4e74615b
Alex Chandel <alex@alex-mbr.northwestern.edu>
2013-03-06 02:30:18 -0600

    shorten & generalize laptop.py code, add packet transmitter!
    PERMANENT REGRESSION: the laptop's rabbitmq-server should be launched
    by laptop.py. Everything should behave normally without this, but don't
    depend on it.
    
    Expect all message queue names & all thread-names to change as I decide
    on appropriate ones.

--- laptop.py
+++ laptop.py
@@ -1,53 +1,36 @@
 #!/usr/bin/env python
 # Copyright Alex Chandel, 2013. All rights reserved.
-import subprocess as sp, consumer, server, multiprocessing as mp, psutil
+import subprocess as sp, consumer, transmitter, server, multiprocessing as mp
 import db, sys, os, signal
 
-class rmq:
-	def __init__(self): self.p = mp.Process()
-	def on(self):
-		if self.p.is_alive():
-			return self.p.pid
-		for ps in psutil.process_iter(): 
-			if ps.name == 'rabbitmq-server': return True
-		return False
-	def start(self, group):
-		if db.ready() and not self.on():
-			self.p = mp.Process(target = lambda: sp.Popen(['rabbitmq-server']).wait())
-			self.p.start()
-	def stop(self):
-		if self.p.is_alive(): sp.Popen(['rabbitmqctl','stop'])
-
-class rmq_consumer:
-	def __init__(self): self.p = mp.Process()
+class task:
+	def __init__(self, runnable, kill = lambda: os.kill(self.p.pid, signal.SIGINT)):
+		self.run = runnable
+		self.kill = kill
+		self.p = mp.Process()
 	def on(self): return self.p.is_alive()
-	def start(self, group):
-		if db.ready() and group[0].on() and not self.on():
-			self.p = mp.Process(target = consumer.receive)
-			self.p.start()
 	def stop(self):
-		if self.on(): os.kill(self.p.pid, signal.SIGINT)
-
-class json_server:
-	def __init__(self): self.p = mp.Process()
-	def on(self): return self.p.is_alive()
-	def start(self, group):
-		if db.ready() and not self.on():
-			self.p = mp.Process(target = server.run)
+		if self.on(): self.kill()
+	def start(self):
+		if not self.on():
+			self.p = mp.Process(target = self.run)
 			self.p.start()
-	def stop(self):
-		if self.on(): os.kill(self.p.pid, signal.SIGINT)
 
-roll = [rmq(), rmq_consumer(), json_server()]
+### NEW TASKS HERE ###
+roll = {'rmq':			task(lambda: sp.Popen(['rabbitmq-server']).wait(), lambda:sp.Popen(['rabbitmqctl','stop'])),
+		'rmq_consumer':	task(consumer.run),
+		'rmq_producer': task(transmitter.run),
+		'json_server':	task(server.run)}
+
 def begin():
-	for worker in roll:
-		worker.start(roll)
-		print worker.on()
+	for key,worker in roll.iteritems():
+		worker.start()
+		print key + " is " + ("on" if worker.on() else "off")
 def quit():
 	for worker in roll: worker.stop()
 
 if __name__ == '__main__':
 	sys.tracebacklimit = 3
-	try: begin() #now chill until all worker Processes terminate
+	try: begin() #chill until all worker Processes terminate
 	except (KeyboardInterrupt, SystemExit): quit()
 	finally: pass
---
+++ transmitter.py
@@ -0,0 +1,19 @@
+import pika
+
+def send():
+	con1 = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	chan1 = con1.channel()
+	chan1.queue_declare(queue='transmitter')
+	con2 = pika.BlockingConnection(pika.ConnectionParameters(host='rpi.chandel.net'))
+	chan2 = con2.channel()
+	chan2.queue_declare(queue='transmitter')
+	def callback(ch, method, properties, pkt):
+		chan2.basic_publish(exchange='',routing_key='transmitter',body=pkt)
+	chan1.basic_consume(callback,queue='transmitter',no_ack=True)
+	try: chan1.start_consuming()
+	except: con1.close(); con2.close()
+
+def run():
+	try: send()
+	except (KeyboardInterrupt, SystemExit): halt=True; time.sleep(4)
+	finally: pass
