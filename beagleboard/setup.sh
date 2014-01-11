49b4861c893febace5f3b7420902c2ed8327f2ef
Alex Chandel <alexchandel@gmail.com>
2014-01-11 17:13:27 -0600

    beagle board info updates

--- beagleboard/setup.sh
+++ beagleboard/setup.sh
@@ -4,15 +4,14 @@
 cd "${HOME}"
 mkdir "/usr/local/bin"
 
-# I assume you already did this:
-# git clone git://github.com/nusolar/sc6-telemetry-code.git
-
 sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install locales
 sudo apt-get install at
 
+
+## configure USB Tethering
 sudo tee "/usr/local/bin/dialer.sh" <<-EOF
 	echo "sudo wvdial" | at now
 EOF
@@ -21,15 +20,9 @@ sudo tee "/etc/udev/rules.d/tmobile.rules" <<-EOF
 	SUBSYSTEM=="usb", ATTR{idVendor}=="19d2", ATTR{idProduct}=="1525", NAME="tmobile", RUN="/usr/local/bin/dialer.sh"
 EOF
 
-sudo apt-get install rabbitmq-server
-sudo apt-get install python3 # currently 3.2.3-6
-sudo apt-get install python3-serial
-sudo apt-get install python3-setuptools
 
-git clone "git://github.com/alexchandel/pika.git"
-cd "pika"
-sudo python3 "setup.py" install
-cd ".."
+## configure telemetry
+git clone git://github.com/nusolar/sc6-telemetry-code.git
 
 sudo tee -a "/etc/inittab"  <<-EOF
 	python3 /home/debian/sc6-telemetry-code/car.py
